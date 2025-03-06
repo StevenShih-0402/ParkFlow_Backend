16e2aa1821cbeeb77c0fe7a64a2d4114e5b2ba88
Steven SHIH <stevenjzshih@ibm.com>
2025-03-06 10:24:59 +0800

    3/6 修正資料庫新增資料 & 回傳訊息 https://trello.com/c/FZcYtPh2/11-%E6%96%B0%E5%A2%9E%E4%BD%BF%E7%94%A8%E8%80%85-05%E5%A4%A9

---
+++ src/main/java/application_operation/ParkFlow/controller/users/payload/UserCreateRq.java
@@ -0,0 +1,42 @@
+package application_operation.ParkFlow.controller.users.payload;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserCreateRq implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @NotBlank
+    @Schema(title = "中文姓名", example = "小吳")
+    private String chineseName;
+
+    @NotBlank
+    @Schema(title = "英文姓名", example = "Wu")
+    private String englishName;
+
+    @Email
+    @NotBlank
+    @Schema(title = "信箱", example = "wu@gmail.com")
+    private String email;
+
+    @NotBlank
+    @Schema(title = "電話號碼", example = "0912567456")
+    private String cellphone;
+
+    @NotBlank
+    @Schema(title = "車牌號碼", example = "DAO-3458")
+    private String carNumber;
+
+    @NotBlank
+    @Schema(title = "車型", example = "TOYOTA")
+    private String carType;
+}
