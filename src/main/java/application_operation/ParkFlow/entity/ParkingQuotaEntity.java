0fe5c54cdd1a700f57794f44eeed12c907030353
Steven SHIH <stevenjzshih@ibm.com>
2025-03-11 17:20:56 +0800

    3/11 完成 修改下週停車數量 API https://trello.com/c/fBvVTfdp/5-%E4%BF%AE%E6%94%B9%E4%B8%8B%E9%80%B1%E5%81%9C%E8%BB%8A%E6%95%B8%E9%87%8F-05%E5%A4%A9
    # Conflicts:
    #	src/main/java/application_operation/ParkFlow/controller/parking/ParkingController.java
    #	src/main/java/application_operation/ParkFlow/exceptionHandler/GlobalExceptionHandler.java
    #	src/main/java/application_operation/ParkFlow/repository/RolesRepository.java
    #	src/main/java/application_operation/ParkFlow/service/ValidUtils.java
    #	src/main/java/application_operation/ParkFlow/service/parking/ParkingService.java
    
    # Conflicts:
    #	src/main/java/application_operation/ParkFlow/dao/parking/ParkingDao.java
    #	src/main/java/application_operation/ParkFlow/repository/ParkingQuotaRepository.java

--- src/main/java/application_operation/ParkFlow/entity/ParkingQuotaEntity.java
+++ src/main/java/application_operation/ParkFlow/entity/ParkingQuotaEntity.java
@@ -21,10 +21,10 @@ public class ParkingQuotaEntity {
     private Integer id;
 
     @Column(name = "WEEK_START_DATE")
-    private LocalDateTime weekStartDate;
+    private LocalDate weekStartDate;
 
     @Column(name = "TOTAL_SLOTS")
-    private Integer totalSlots;
+    private Integer totalSlots = 1;
 
     @Column(name = "CREATED_AT", insertable = false)
     private LocalDateTime createdAt;
